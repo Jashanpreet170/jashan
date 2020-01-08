be0fd9cfd9b148ca56888033e47b33cc17a5b5d0
Jashanpreet170 <55210251+Jashanpreet170@users.noreply.github.com>
2020-01-08 12:51:25 -0500

    Add files via upload

---
+++ Week1/src/week1/Week1.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package week1;
+
+/**
+ * @author 758504
+ *
+ */
+public class Week1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+         
+		
+		//Declare Values
+		
+		String name = "Jashanpreet Singh";
+		int age = 19;
+		//Print my name
+		System.out.println("My name is "+name + " and my age is "+ age );
+		
+	}
+
+}
